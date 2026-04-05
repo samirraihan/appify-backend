da21f269bb4ec674ac0107152f230ea9dac1f0de
samirraihan <samirraihan93@gmail.com>
2026-04-05 14:43:11 +0600

    Add LoginRequest and RegisterRequest classes for user authentication

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => ['required', Password::defaults()],
+        ];
+    }
+}
